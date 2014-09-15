53ba4481ea75bd7affe278cd1041d7bd8a63d826
denisnarush <child.denis@gmail.com>
2014-09-15 18:50:08 +0300

    clear

--- src/range.js
+++ src/range.js
@@ -1,40 +1,51 @@
-// constructor
-var RangeJS = function (element, options) {
-    this.labelsPosition = [];
-    this.container = document.querySelector(element);
+(function () {
+    // constructor
+    var RangeJS = function (element, options) {
 
-    //set options
-    makeOptions.call(this, options, RangeJS.defaults);
-    //render elements
-    render.call(this);
-    //bind handlers for elements
-    this.nodes.root.addEventListener('mousedown', onRootMouseDown);
-    document.addEventListener('mouseup', onDocumentMouseUp);
+        /* --------
+         * if no element passed
+         * create new element
+        */
+        if (!element) {
+            element = document.createElement('div');)
+        }
 
-    this.getValue = RangeJS.prototype.getValue;
-    this.destroy = RangeJS.prototype.destroy;
-    this.onValueChange = function () {};
-};
 
-// default options
-RangeJS.defaults = {
-    min: 3,
-    max: 13,
-    value: 5,
-    step: false,
-    labelsClassName: 'RangeJS-label',
-    onValueChange: function () { }
-};
+        // this.labelsPosition = [];
+        // this.container = document.querySelector(element);
 
-// public
-RangeJS.prototype = {
-    getValue: function () {
-        return this.options.value;
-    },
-    destroy: function () {
-        this.nodes.root.removeEventListener('mousedown', onRootMouseDown);
-        this.nodes.root.parentNode.removeChild(this.nodes.root);
-    }
-};
+        //set options
+        // makeOptions.call(this, options, RangeJS.defaults);
+        //render elements
+        // render.call(this);
+        //bind handlers for elements
+        // this.nodes.root.addEventListener('mousedown', onRootMouseDown);
+        // document.addEventListener('mouseup', onDocumentMouseUp);
 
-window.Range = RangeJS;
+        // this.getValue = RangeJS.prototype.getValue;
+        // this.destroy = RangeJS.prototype.destroy;
+        // this.onValueChange = function () {};
+    };
+
+    // default options
+    RangeJS.defaults = {
+        min: 3,
+        max: 13,
+        value: 5,
+        step: false,
+        labelsClassName: 'RangeJS-label',
+        onValueChange: function () { }
+    };
+
+    // public
+    RangeJS.prototype = {
+        getValue: function () {
+            return this.options.value;
+        },
+        destroy: function () {
+            // destroy all events, html
+        }
+    };
+
+    Range = RangeJS;
+})()
